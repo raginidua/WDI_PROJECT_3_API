6f48398dc98c3f12ad022d4e53abf9a41cee533b
raginidua <raginidua1@gmail.com>
2017-02-21 19:44:50 +0000

    updates

--- app/serializers/bid_serializer.rb
+++ app/serializers/bid_serializer.rb
@@ -1,9 +1,13 @@
 class BidSerializer < ActiveModel::Serializer
-  attributes :id, :price, :bidder
+  attributes :id, :price, :bidder, :work
   has_one :user
   has_one :piece
 end
 
 def bidder
   UserSerializer.new(self.user).attributes
+end
+
+def work
+  PieceSerializer.new(self.piece).attributes
 end
--- app/serializers/piece_serializer.rb
+++ app/serializers/piece_serializer.rb
@@ -1,5 +1,5 @@
 class PieceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :title, :description, :minimum_bid, :status, :closing_time, :image
+  attributes :id, :title, :description, :minimum_bid, :status, :closing_time, :image, :bids
   has_one :user
   has_many :bids
 end
