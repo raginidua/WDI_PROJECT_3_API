b15a4a9644f836131c26924ef7e3059f5244d6c9
raginidua <raginidua1@gmail.com>
2017-02-22 11:25:44 +0000

    Updates

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :role, :art_school
+  attributes :id, :first_name, :last_name, :username, :email, :role, :art_school, :bids
   has_many :pieces
   has_many :bids
 end
